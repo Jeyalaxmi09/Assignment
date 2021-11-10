fb875d776b3ebfdd28a31272bbdc708ce1231903
Jeyalaxmi09 <93927844+Jeyalaxmi09@users.noreply.github.com>
2021-11-10 21:49:07 +0530

    Add files via upload

---
+++ odd even.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+count_even=0
+count_odd=0
+for i in range(1,10):
+    if(i%2==0):
+        count_even+=1
+    else:
+        count_odd+=1
+print("Number of even numbers",count_even)
+print("Number of odd number",count_odd)
+
+
+# In[ ]:
+
+
+
+
