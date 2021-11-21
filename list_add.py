ce87db99bdee5db1152e8828ea2f73c33cb2a085
Jeyalaxmi09 <93927844+Jeyalaxmi09@users.noreply.github.com>
2021-11-21 23:34:58 +0530

    Add files via upload

---
+++ list_add.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def sume(numbers):
+    total = 0
+    for x in numbers:
+        total=total+x
+    return total
+print(sume([8, 2, 3, 0, 7]))
+
+
+# In[ ]:
+
+
+
+
